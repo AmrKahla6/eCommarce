19c97d07fc492b7ca20db6c78e5f721cb77fef5b
AmrKahla6 <amrkahla6@gmail.com>
2020-12-06 18:21:54 +0200

    Filter & Validate Inpute Signup

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start();
     session_start();
 
     $pageTitle = "Login";
@@ -14,32 +15,76 @@
      */
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        $username   = $_POST['username'];
-        $password   = $_POST['password'];
-        $hashedPass = sha1($password);
-
-        //Check if user exist in db
-        $stmt  = $con->prepare("SELECT
-                                     Username , Password
-                                FROM
-                                    users
-                                where
-                                    Username = ?
-                                AND
-                                    Password = ?");
-        $stmt->execute(array($username , $hashedPass));
-
-        //if count > 0 this mean the db contain record about this username
-        $count = $stmt->rowCount(); // exist or not
-        if($count > 0)
+        if(isset($_POST['login']))
         {
-            $_SESSION['user'] = $username;
-            // print_r($_SESSION['user']);
-            header('Location: index.php');
-            exit();
+            $username   = $_POST['username'];
+            $password   = $_POST['password'];
+            $hashedPass = sha1($password);
+
+            //Check if user exist in db
+            $stmt  = $con->prepare("SELECT
+                                        Username , Password
+                                    FROM
+                                        users
+                                    where
+                                        Username = ?
+                                    AND
+                                        Password = ?");
+            $stmt->execute(array($username , $hashedPass));
+
+            //if count > 0 this mean the db contain record about this username
+            $count = $stmt->rowCount(); // exist or not
+            if($count > 0)
+            {
+                $_SESSION['user'] = $username;
+                // print_r($_SESSION['user']);
+                header('Location: index.php');
+                exit();
+            }
+        }
+        else
+        {
+            $formErrors = array();
+
+            //Username Filter & Errors
+            if(isset($_POST['username']))
+            {
+                $filterUser = filter_var($_POST['username'] , FILTER_SANITIZE_STRING);
+                if(strlen($filterUser) < 4)
+                {
+                    $formErrors[] = 'Username Must Be Larger Than 4 Characters';
+                }
+            }
+
+            //Password Filter & Errors
+            if(isset($_POST['password']) && isset($_POST['password-confermation']))
+            {
+                if(empty($_POST['password']))
+                {
+                    $formErrors[] = 'Password Can\'t Be Null';
+                }
+
+                $pass1 =  sha1($_POST['password']);
+                $pass2 =  sha1($_POST['password-confermation']);
+
+                if($pass1 !== $pass2)
+                {
+                    $formErrors[] = 'Password Is\'t Match';
+                }
+            }
+
+
+              //Email Filter & Errors
+              if(isset($_POST['email']))
+              {
+                  $filterEmail = filter_var($_POST['email'] , FILTER_SANITIZE_EMAIL);
+                  if(filter_var($filterEmail , FILTER_VALIDATE_EMAIL) != true)
+                  {
+                      $formErrors[] = 'This Email Is Not Valid';
+                  }
+              }
         }
     }
-
 
 ?>
 
@@ -49,34 +94,58 @@
         </h1>
         <!-- Start Form For Login -->
         <form class="login" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="form1">
-            <input class="form-control" type="text"     name="username" placeholder = "Enter Your Username" autocomplete="off">
+            <input class="form-control"          type="text"     name="username" placeholder = "Enter Your Username" autocomplete="off">
             <input class="password form-control" type="password" name="password" placeholder = "Enter Your Password" autocomplete="new-password">
-            <i class="show-pass fa fa-eye fa-2x"></i>
-            <input class="btn-primary btn-block"  type="submit"   value="Login">
+            <input class="btn-primary btn-block"  type="submit"  name="login"   value="Login">
+            <i     class="show-pass fa fa-eye fa-2x"></i>
         </form>
         <!-- End Form For Login -->
 
         <!-- Start Form For Signup -->
         <form class="signup" action="" method="post" id="form2">
             <div class="input-container">
-                <input class="form-control" type="text"     name="username"              placeholder = "Enter Username"            autocomplete="off"          required>
+                <input pattern=".{4,}" title="Username must be 4 characters"
+                       class="form-control" type="text" name="username"
+                       placeholder = "Enter Username"  autocomplete="off" required>
             </div>
 
             <div class="input-container">
-                <input class="form-control" type="password" name="password"              placeholder = "Enter A Complex Password"  autocomplete="new-password" required>
+                <input minlength="4" class="form-control"
+                       type="password" name="password"
+                       placeholder = "Enter A Complex Password"
+                       autocomplete="new-password" required>
             </div>
 
             <div class="input-container">
-                <input class="form-control" type="password" name="password-confermation" placeholder = "Conferm Your Password"     autocomplete="new-password" required>
+                <input  minlength="4"  class="form-control"
+                        type="password" name="password-confermation"
+                        placeholder = "Conferm Your Password"
+                        utocomplete="new-password" required>
             </div>
 
             <div class="input-container">
-                <input class="form-control" type="email"    name="email"                 placeholder = "Enter Your Valid Email"    autocomplete="off"          required>
+                <input class="form-control" type="email"
+                       name="email"   placeholder = "Enter Your Valid Email"
+                       autocomplete="off" required >
             </div>
-                <input class="btn-success btn-block"  type="submit"   value="Signup">
+                <input class="btn-success btn-block"  type="submit" name="signup"    value="Signup">
         </form>
         <!-- End Form For Signup -->
+        <div class="the-errors text-center">
+             <?php
+                if(!empty($formErrors))
+                {
+                    foreach($formErrors as $error)
+                    {
+                        echo $error . '<br>';
+                    }
+                }
+             ?>
+        </div>
     </div>
 
-<?php include $tpl . "footer.php"; ?>
+<?php
+include $tpl . "footer.php";
+ob_end_flush();
+?>
 
